8788c8a5bfb915b2854719a87b6f3c0731266528
jackf723 <jackf723@gmail.com>
2023-07-31 17:11:08 -0500

    re-added mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://pneumasolutions.github.io',
   base: '/rim-manual-ng',
   integrations: [mdx()]
-
-})
+});
