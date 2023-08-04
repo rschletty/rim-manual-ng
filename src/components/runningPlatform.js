6bbf352887a05bd3244d224e55105949189e5a7c
jackf723 <jackf723@gmail.com>
2023-08-04 10:38:55 -0500

    working platform detection!
    - Platform detection now officially works! Now using Markdown only in sections that do not have platform code. We are using pure html in the form of .astro files in order to leverage full js scripting ability rather than the evidently unstable nature of JSX in MDX.

--- src/components/runningPlatform.js
+++ src/components/runningPlatform.js
@@ -1,14 +1,38 @@
-// platform detection code
-const platform = (() => {
-    const userAgent = window.navigator.userAgent;
-    if (userAgent.includes("Win")) {
-      return "Windows";
-    } else if (userAgent.includes("Mac")) {
-      return "Mac";
+// components/runningPlatform.js
+// platform detection function
+function getPlatform() {
+    const userAgent = navigator.userAgent.toLowerCase();
+    if (userAgent.indexOf('win') !== -1) {
+        return 'Windows';
+    } else if (userAgent.indexOf('mac') !== -1) {
+        return 'Mac';
     } else {
-      return "Unknown";
+        return 'Unknown';
     }
-  })();
-  
-  export default platform;
-  
+}
+
+// Function to unhide the content div based on the platform
+function showPlatformRequirements() {
+    const platform = getPlatform();
+
+    // Get all the content divs
+    const windowsRequirements= document.getElementById('windows-requirements');
+    const macRequirements = document.getElementById('mac-requirements');
+    const unknownContent = document.getElementById('unknown-content');
+
+    // Show the relevant content div based on the platform
+    switch (platform) {
+        case 'Windows':
+            windowsRequirements.style.display = 'block';
+            break;
+        case 'Mac':
+            macRequirements.style.display = 'block';
+            break;
+        default:
+            unknownContent.style.display = 'block';
+            break;
+    }
+}
+
+// Call the showPlatformSpecificContent function once the page is loaded
+window.addEventListener('load', showPlatformRequirements);
