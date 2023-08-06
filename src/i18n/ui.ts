5312357e9b96b0799cc6c6c5fab5cf995174b02f
jackf723 <jackf723@gmail.com>
2023-08-06 12:25:56 -0500

    ui strings translated

---
+++ src/i18n/ui.ts
@@ -0,0 +1,32 @@
+export const languages = {
+  en: 'en-US',
+  es: 'es',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+  en: {
+    'nav.welcome': 'Welcome',
+    'nav.systemspecs': 'System Requirements',
+    'nav.installation': 'Installation',
+    'nav.gettingconnected': 'Getting Connected',
+    'nav.workingwithsessions': 'Working within a Session',
+    'nav.dashboard': 'The RIM Dashboard',
+    'nav.faq': 'Frequently Asked Questions',
+    'nav.pricing': 'Pricing',
+    'nav.changelog': 'Change Log',
+  },
+  es: {
+    'nav.welcome': 'Bienvenido',
+    'nav.systemspecs': 'Requisitos del sistema',
+    'nav.installation': 'Instalación',
+    'nav.gettingconnected': 'Conectarse',
+    'nav.workingwithsessions': 'Trabajar en una sesión',
+    'nav.dashboard': 'The Cuadro de mandos de RIM',
+    'nav.faq': 'Preguntas frecuentes',
+    'nav.pricing': 'Precios',
+    'nav.changelog': 'Registro de cambios',
+
+  },
+} as const;
